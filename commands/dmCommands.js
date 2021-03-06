73336876ef41425f6d0a05b853a39bb6e37db383
meowsome <tycarr01@gmail.com>
2021-03-05 22:06:47 -0500

    Update initial dm instructions message

--- commands/dmCommands.js
+++ commands/dmCommands.js
@@ -6,7 +6,7 @@ module.exports = {
         //Validate reaction is correct emoji
         if (reaction.emoji.name != process.env.welcomeEmoji) return;
 
-        user.send("Make a copy of this Google Document by selecting the file dropdown on the top left of the screen and click on \"Make a copy\". Once you’ve done that fill out the application below.\nhttps://docs.google.com/document/d/16ZFiYO2aLMMTSP0eFejfj1kXdz3AtgmPqP7MUn38xAQ/edit?usp=sharing\n\nOnce you are done filling out the application make a sharable link and post that link here.\n\nYou are only allowed to send a message in that channel once every 6 hours, so if you mess up that format your application will be ignored.");
+        user.send("1. Open the google document link below.\n2. Click on \"Make a copy\".\n3. Once you’ve done that fill out the application.\n4. Once you have completed your application, change the document's sharing settings on the top right to \"anyone with the link can view\", and send it back here.\n\nYou are only allowed to apply once every 6 hours, so if you mess up that format your application will be ignored.\n\nhttps://docs.google.com/document/d/16ZFiYO2aLMMTSP0eFejfj1kXdz3AtgmPqP7MUn38xAQ/copy");
     },
 
     handleGoogleDocDM: function(client, message) {
